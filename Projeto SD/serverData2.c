84c33ca473507543c5abf78146bd3ec4bc2a526a
Adriano Munin <adrianomunin@gmail.com>
2020-11-08 22:02:07 -0300

    BugFix

--- Projeto SD/serverData2.c
+++ Projeto SD/serverData2.c
@@ -16,33 +16,33 @@
 
 #define NOME_SEMAFORO "/semaforo2"
 #define PORTA_SERVER_DATA 6000
-#define SHM_KEY 6000
+#define SHM_KEY 5000
 
 void modificaDado(char *dado, sem_t *semaphore, char *share_memory);
 
 int main(int argc, char **argv)
 {
-    unsigned short port;       
-    char sendbuf[1024];              
-    char recvbuf[1024];              
-    struct sockaddr_in client; 
-    struct sockaddr_in server; 
-    int s;                     /* Socket para aceitar conexoes       */
-    int ns;                    /* Socket conectado ao cliente        */
+    unsigned short port;
+    char sendbuf[1024];
+    char recvbuf[1024];
+    struct sockaddr_in client;
+    struct sockaddr_in server;
+    int s;  /* Socket para aceitar conexoes       */
+    int ns; /* Socket conectado ao cliente        */
     int shmid;
     int namelen;
     pid_t pid, fid;
 
-    
     //Criando memoria compartilhada
-    shmid = shmget(SHM_KEY, 1024, IPC_CREAT |  0666);
-    if (shmid < 0) {
+    shmid = shmget(SHM_KEY, 1024, IPC_CREAT | 0666);
+    if (shmid < 0)
+    {
         printf("shmget error\n");
     }
-    
+
     //Associando memoria compartilhda a um ponteiro
-    char *share_memory = (char*)shmat(shmid, NULL, 0);
-    
+    char *share_memory = (char *)shmat(shmid, NULL, 0);
+
     //Inicializando a memoria compartilhada com um valor qualquer
     strcpy(share_memory, "NADA");
 
@@ -51,7 +51,7 @@ int main(int argc, char **argv)
 
     //Criando semaforo
     sem_t *semaphore = sem_open(NOME_SEMAFORO, O_CREAT, 0644, 0);
-    if(semaphore == SEM_FAILED)
+    if (semaphore == SEM_FAILED)
     {
         perror("Erro ao inicializar semaforo");
         exit(EXIT_FAILURE);
@@ -62,17 +62,17 @@ int main(int argc, char **argv)
      */
     if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0)
     {
-	  perror("Socket()");
-	  exit(2);
+        perror("Socket()");
+        exit(2);
     }
 
-   /*
+    /*
     * Define a qual endereco IP e porta o servidor estara ligado.
     * IP = INADDDR_ANY -> faz com que o servidor se ligue em todos
     * os enderecos IP
     */
-    server.sin_family = AF_INET;   
-    server.sin_port   = htons(PORTA_SERVER_DATA);       
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORTA_SERVER_DATA);
     server.sin_addr.s_addr = INADDR_ANY;
 
     /*
@@ -80,8 +80,8 @@ int main(int argc, char **argv)
      */
     if (bind(s, (struct sockaddr *)&server, sizeof(server)) < 0)
     {
-	perror("Bind()");
-	exit(3);
+        perror("Bind()");
+        exit(3);
     }
 
     /*
@@ -90,106 +90,108 @@ int main(int argc, char **argv)
      */
     if (listen(s, 1) != 0)
     {
-	  perror("Listen()");
-	  exit(4);
+        perror("Listen()");
+        exit(4);
     }
 
-    while(1)
+    while (1)
     {
-	  /*
+        /*
 	  * Aceita uma conexao e cria um novo socket atraves do qual
 	  * ocorrera a comunicacao com o cliente.
 	  */
-	  namelen = sizeof(client);
-	  if ((ns = accept(s, (struct sockaddr *) &client, (socklen_t *) &namelen)) == -1)
-	  {
-		perror("Accept()");
-		exit(5);
-	  }
-	  
-	  if ((pid = fork()) == 0) 
-      {
-		/*
+        namelen = sizeof(client);
+        if ((ns = accept(s, (struct sockaddr *)&client, (socklen_t *)&namelen)) == -1)
+        {
+            perror("Accept()");
+            exit(5);
+        }
+
+        if ((pid = fork()) == 0)
+        {
+            /*
 		 * Processo filho 
 		 */
-	      
-		/* Fecha o socket aguardando por conexoes */
-		close(s);
 
-		/* Processo filho obtem seu proprio pid */
-		fid = getpid();
+            /* Fecha o socket aguardando por conexoes */
+            close(s);
 
-		/* Recebe uma mensagem do cliente atraves do novo socket conectado */
-		if (recv(ns, recvbuf, sizeof(recvbuf), 0) == -1)
-		{
-		    perror("Recv()");
-		    exit(6);
-		}
+            /* Processo filho obtem seu proprio pid */
+            fid = getpid();
 
-        if (strcmp(recvbuf, "write") == 0)
-        {
+            /* Recebe uma mensagem do cliente atraves do novo socket conectado */
             if (recv(ns, recvbuf, sizeof(recvbuf), 0) == -1)
-		    {
-		        perror("Recv()");
-		        exit(6);
-		    }
-            modificaDado(recvbuf, semaphore, share_memory);
-            printf("%s \n", share_memory);
+            {
+                perror("Recv()");
+                exit(6);
+            }
+
+            if (strcmp(recvbuf, "write") == 0)
+            {
+                if (recv(ns, recvbuf, sizeof(recvbuf), 0) == -1)
+                {
+                    perror("Recv()");
+                    exit(6);
+                }
+                modificaDado(recvbuf, semaphore, share_memory);
+                printf("Dado recebido: %s \n", share_memory);
+            }
+            else if (strcmp(recvbuf, "read") == 0)
+            {
+                strcpy(sendbuf, share_memory);
+                /* Envia uma mensagem ao cliente atraves do socket conectado */
+                if (send(ns, sendbuf, strlen(sendbuf) + 1, 0) < 0)
+                {
+                    perror("Send()");
+                    exit(7);
+                }
+                printf("Dado enviado: %s \n", sendbuf);
+            }
+
+            /* Fecha o socket conectado ao cliente */
+            close(ns);
+            exit(0);
         }
-        else if(strcmp(recvbuf, "read") == 0)
+        else
         {
-            strcpy(sendbuf, share_memory);
-		    /* Envia uma mensagem ao cliente atraves do socket conectado */
-		    if (send(ns, sendbuf, strlen(sendbuf)+1, 0) < 0)
-		    {
-		        perror("Send()");
-		        exit(7);
-		    }
-        }
-        
-		/* Fecha o socket conectado ao cliente */
-		close(ns);
-		exit(0);
-	  }
-	  else
-	  {  
-		/*
+            /*
 		 * Processo pai 
 		 */
-		
-		if (pid > 0)
-		{
-		    printf("Processo filho criado: %d\n", pid);
 
-            /* Detach the shared memory segment.  */ 
-            shmdt (share_memory); 
+            if (pid > 0)
+            {
+                printf("Processo filho criado: %d\n", pid);
 
-            /* Deallocate the shared memory segment.  */ 
-            shmctl (shmid, IPC_RMID, 0);
-
-            /*Fechando o semaforo*/
-            sem_close(semaphore);
-
-		    /* Fecha o socket conectado ao cliente */
-		    close(ns);
-		}
-		else
-		{
-		    perror("Fork()");
-		    exit(7);	      
-		}
-	  }
+                /* Fecha o socket conectado ao cliente */
+                close(ns);
+            }
+            else
+            {
+                perror("Fork()");
+                exit(7);
+            }
+        }
     }
+
+    /* Detach the shared memory segment.  */
+    shmdt(share_memory);
+
+    /* Deallocate the shared memory segment.  */
+    shmctl(shmid, IPC_RMID, 0);
+
+    /*Fechando o semaforo*/
+    sem_close(semaphore);
 }
 
 void modificaDado(char *dado, sem_t *semaphore, char *share_memory)
 {
     //Travando o semaforo
-    sem_wait(semaphore);
+    //sem_wait(semaphore);
 
     //Copiando os dados requisitados para a memoria compartilhada
     strcpy(share_memory, dado);
+    printf("dsjnlk.f");
 
     //Liberando o semaforo
-    sem_post(semaphore);
+    //sem_post(semaphore);
 }
